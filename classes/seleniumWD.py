bf7e9b443dfe11df18eeffa202a35473fc42e8a6
Artur vinicius umbelino barbalho <arthurvinicius907@gmail.com>
2023-03-18 22:34:45 -0300

    set driver path

--- classes/seleniumWD.py
+++ classes/seleniumWD.py
@@ -2,13 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import os
 
 class WD(webdriver.Chrome):
     def __init__(self, link, driver_path= r"C:/Selenium Driver"):
         self.link = link
         options = Options()
         options.add_experimental_option("detach", True)
-        self.driver_path = driver_path
+        os.environ['PATH'] += driver_path
         super(WD, self).__init__(options=options)
         self.implicitly_wait(5)
         self.get(self.link) 
